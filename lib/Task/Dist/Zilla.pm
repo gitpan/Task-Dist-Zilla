144b96c2ef15a9f6930b9ff424c51301153b061b
Jerome Quelin <jquelin@cpan.org>
2010-01-07 12:56:33 +0000

    Import of JQUELIN/Task-Dist-Zilla-1.100070 from CPAN.
    gitpan-cpan-distribution: Task-Dist-Zilla
    gitpan-cpan-version:      1.100070
    gitpan-cpan-path:         JQUELIN/Task-Dist-Zilla-1.100070.tar.gz
    gitpan-cpan-author:       JQUELIN
    gitpan-cpan-maturity:     released

---
+++ lib/Task/Dist/Zilla.pm
@@ -0,0 +1,244 @@
+# 
+# This file is part of Task-Dist-Zilla
+# 
+# This software is copyright (c) 2010 by Jerome Quelin.
+# 
+# This is free software; you can redistribute it and/or modify it under
+# the same terms as the Perl 5 programming language system itself.
+# 
+use 5.008;
+use strict;
+use warnings;
+
+package Task::Dist::Zilla;
+our $VERSION = '1.100070';
+# ABSTRACT: task to install dist-zilla and all its plugins
+
+1;
+
+
+
+
+
+
+=pod
+
+=head1 NAME
+
+Task::Dist::Zilla - task to install dist-zilla and all its plugins
+
+=head1 VERSION
+
+version 1.100070
+
+=head1 SYNOPSIS
+
+    perl -MCPANPLUS -e 'install Task::Dist::Zilla'
+
+=head1 DESCRIPTION
+
+This task is merely a placeholder to pull all dist-zilla related modules
+in one go.
+
+=head1 TASK CONTENTS
+
+=head2 Base application
+
+=head3 Dist::Zilla
+
+=head2 Plugins
+
+=head3 Dist::Zilla::Plugin::AllFiles
+
+=head3 Dist::Zilla::Plugin::AutoPrereq
+
+=head3 Dist::Zilla::Plugin::AutoVersion
+
+=head3 Dist::Zilla::Plugin::AutoVersion::Relative
+
+=head3 Dist::Zilla::Plugin::BumpVersion
+
+=head3 Dist::Zilla::Plugin::CheckChangeLog
+
+=head3 Dist::Zilla::Plugin::CompileTests
+
+=head3 Dist::Zilla::Plugin::CopyTo
+
+=head3 Dist::Zilla::Plugin::CriticTests
+
+=head3 Dist::Zilla::Plugin::ExtraTests
+
+=head3 Dist::Zilla::Plugin::FakeRelease
+
+=head3 Dist::Zilla::Plugin::Git
+
+=head3 Dist::Zilla::Plugin::Git::Check
+
+=head3 Dist::Zilla::Plugin::Git::Commit
+
+=head3 Dist::Zilla::Plugin::Git::Push
+
+=head3 Dist::Zilla::Plugin::Git::Tag
+
+=head3 Dist::Zilla::Plugin::InlineFiles
+
+=head3 Dist::Zilla::Plugin::InstallDirs
+
+=head3 Dist::Zilla::Plugin::License
+
+=head3 Dist::Zilla::Plugin::MakeMaker
+
+=head3 Dist::Zilla::Plugin::MakeMaker::SkipInstall
+
+=head3 Dist::Zilla::Plugin::MakeMaker::Test
+
+=head3 Dist::Zilla::Plugin::Manifest
+
+=head3 Dist::Zilla::Plugin::ManifestSkip
+
+=head3 Dist::Zilla::Plugin::MatchManifest
+
+=head3 Dist::Zilla::Plugin::MetaJSON
+
+=head3 Dist::Zilla::Plugin::MetaProvides
+
+=head3 Dist::Zilla::Plugin::MetaProvides::Class
+
+=head3 Dist::Zilla::Plugin::MetaProvides::FromFile
+
+=head3 Dist::Zilla::Plugin::MetaProvides::Package
+
+=head3 Dist::Zilla::Plugin::MetaRecommends
+
+=head3 Dist::Zilla::Plugin::MetaResources
+
+=head3 Dist::Zilla::Plugin::MetaTests
+
+=head3 Dist::Zilla::Plugin::MetaYAML
+
+=head3 Dist::Zilla::Plugin::ModuleBuild
+
+=head3 Dist::Zilla::Plugin::ModuleBuild::Test
+
+=head3 Dist::Zilla::Plugin::ModuleInstall
+
+=head3 Dist::Zilla::Plugin::NextRelease
+
+=head3 Dist::Zilla::Plugin::PerlTidy
+
+=head3 Dist::Zilla::Plugin::PkgVersion
+
+=head3 Dist::Zilla::Plugin::PodLoom
+
+=head3 Dist::Zilla::Plugin::PodPurler
+
+=head3 Dist::Zilla::Plugin::PodTests
+
+=head3 Dist::Zilla::Plugin::PodVersion
+
+=head3 Dist::Zilla::Plugin::PodWeaver
+
+=head3 Dist::Zilla::Plugin::Prepender
+
+=head3 Dist::Zilla::Plugin::Prereq
+
+=head3 Dist::Zilla::Plugin::PruneCruft
+
+=head3 Dist::Zilla::Plugin::PruneFiles
+
+=head3 Dist::Zilla::Plugin::Readme
+
+=head3 Dist::Zilla::Plugin::ReadmeFromPod
+
+=head3 Dist::Zilla::Plugin::Repository
+
+=head3 Dist::Zilla::Plugin::TaskWeaver
+
+=head3 Dist::Zilla::Plugin::TemplateCJM
+
+=head3 Dist::Zilla::Plugin::UploadToCPAN
+
+=head3 Dist::Zilla::Plugin::VersionFromModule
+
+=head2 Plugin bundles
+
+=head3 Dist::Zilla::PluginBundle::CJM
+
+=head3 Dist::Zilla::PluginBundle::Classic
+
+=head3 Dist::Zilla::PluginBundle::FAYLAND
+
+=head3 Dist::Zilla::PluginBundle::Filter
+
+=head3 Dist::Zilla::PluginBundle::Git
+
+=head3 Dist::Zilla::PluginBundle::JQUELIN
+
+=head3 Dist::Zilla::PluginBundle::RJBS
+
+=head1 NAME
+
+Task::Dist::Zilla - task to install dist-zilla and all its plugins
+
+=head1 VERSION
+
+version 1.100070
+
+=head1 SEE ALSO
+
+You can find more information on this module at:
+
+=over 4
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Task-Dist-Zilla>
+
+=item * See open / report bugs
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Task-Dist-Zilla>
+
+=item * Mailing-list (same as dist-zilla)
+
+L<http://www.listbox.com/subscribe/?list_id=139292>
+
+=item * Git repository
+
+L<http://github.com/jquelin/task-dist-zilla.git>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Task-Dist-Zilla>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Task-Dist-Zilla>
+
+=back
+
+=head1 AUTHOR
+
+  Jerome Quelin
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Jerome Quelin.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 AUTHOR
+
+  Jerome Quelin
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Jerome Quelin.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
+__END__
